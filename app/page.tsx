003b222d3c5a66414e22fba12ff9729e0f0f847f
DMAKCODESERIES <daniyalabbasi0349@gmail.com>
2024-11-16 22:46:17 +0500

    helonn

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
-'use client'
+// 'use client'
 
+
+import TelegramAuth from '@/components/telegramAuth';
+import { getSession } from '@/utils/session';
 import WebApp from '@twa-dev/sdk'
-import { useEffect, useState } from 'react'
+// import { useEffect, useState } from 'react'
 
 // Define the interface for user data
 interface UserData {
@@ -13,18 +16,22 @@ interface UserData {
   is_premium?: boolean;
 }
 
-export default function Home() {
-  const [userData, setUserData] = useState<UserData | null>(null)
+export default async function Home() {
+  const session = await getSession()
+  // const [userData, setUserData] = useState<UserData | null>(null)
 
-  useEffect(() => {
-    if (WebApp.initDataUnsafe.user) {
-      setUserData(WebApp.initDataUnsafe.user as UserData)
-    }
-  }, [])
+  // useEffect(() => {
+  //   if (WebApp.initDataUnsafe.user) {
+  //     setUserData(WebApp.initDataUnsafe.user as UserData)
+  //   }
+  // }, [])
 
   return (
     <main className="p-4 items-center bg-red">
-      {userData ? (
+          <h1 className="text-4xl font-bold mb-8">Jwt Authentication for Telegram Mini Apps</h1>
+      <pre>{JSON.stringify(session, null, 2)}</pre>
+      <TelegramAuth/>
+      {/* {userData ? (
         <>
           <h1 className="text-2xl font-bold mb-4">User Data</h1>
           <ul>
@@ -38,7 +45,7 @@ export default function Home() {
         </>
       ) : (
         <div>Loading...</div>
-      )}
+      )} */}
     </main>
   )
 }
